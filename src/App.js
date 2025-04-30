18f99fc434e4fb889b84f8995570f7594071a055
Bernardo Pozzebon Poluceno <162700391+Bernardo-03P@users.noreply.github.com>
2025-04-30 11:16:15 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Adestramento from './pages/Adestramento';
+import BanhoTosa from './pages/BanhoTosa';
+import HotelResort from './pages/HotelResort'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home />} /> 
+        <Route path='/banho-e-tosa' element={ <BanhoTosa />}  />
+        <Route path='/hotel-resort' element={ <HotelResort />}/>
+        <Route path='/adestramento' element={ <Adestramento />}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
