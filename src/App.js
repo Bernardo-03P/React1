e826c8a36ee7f8855061c3cc7bb2442cb00ff183
Bernardo Pozzebon Poluceno <162700391+Bernardo-03P@users.noreply.github.com>
2025-05-07 11:22:34 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Home from './pages/Home';
 import Adestramento from './pages/Adestramento';
 import BanhoTosa from './pages/BanhoTosa';
 import HotelResort from './pages/HotelResort'
+import NavBarMenu from './pages/NavBarMenu';
 
 function App() {
   return (
     <Router>
+      <header> <NavBarMenu /></header>
+    
+    <main>
       <Routes>
         <Route path='/' element={ <Home />} /> 
         <Route path='/banho-e-tosa' element={ <BanhoTosa />}  />
         <Route path='/hotel-resort' element={ <HotelResort />}/>
         <Route path='/adestramento' element={ <Adestramento />}/>
       </Routes>
+      </main>
+      <footer>
+        
+      </footer>
     </Router>
   );
 }
